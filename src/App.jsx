432921b68f1cfb7503900a2847f7e4efef909f83
gus-p3 <sinnamexd3432@gmail.com>
2026-03-09 20:29:56 -0600

    Feat: Primer commit, este contiene el ejercicio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import PedidoForm from './componentes/PedidoForm'
+
+
+function App() {
+  return (
+    <div>
+      <h1>Pedidos</h1>
+      <PedidoForm />
+    </div>)
+}
+
+
+export default App
+
